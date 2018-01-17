f69c3d0d575d80deec7bfc06c9fa380813bcc328
Steve Chan <sychan168@gmail.com>
2018-01-17 14:35:42 -0800

    Tweak java options to enable headless mode

--- deployment/bin/search_tools.sh
+++ deployment/bin/search_tools.sh
@@ -4,9 +4,9 @@ MIN_MEM=${max_mem:-"1000"}
 MAX_MEM=${min_mem:-"3000"}
 
 # Setup the JVM options using the JDK 1.7+ environment var _JAVA_OPTIONS
-export _JAVA_OPTIONS="-Xms${MIN_MEM}m -Xmx${MAX_MEM}m -XX:+UseG1GC"
+export _JAVA_OPTIONS="-Djava.awt.headless=true -Xms${MIN_MEM}m -Xmx${MAX_MEM}m -XX:+UseG1GC"
 
 cd /kb/deployment/services/search/tomcat/webapps/
 unzip ROOT.war
-java -cp "WEB-INF/lib/*"  kbasesearchengine.tools.SearchTools $@
+java -server -cp "WEB-INF/lib/*"  kbasesearchengine.tools.SearchTools $@
     
