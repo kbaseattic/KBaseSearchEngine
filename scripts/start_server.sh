9a6aabeda6a4469f3c15c805949d808ca69804e3
Steve Chan <sychan168@gmail.com>
2018-01-25 14:43:26 -0800

    Fixes based on PR128

--- scripts/start_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-script_dir=$(dirname "$(readlink -f "$0")")
-cd scripts
-java -Xmx1g -cp /Users/sychan/src/jars/lib/jars/jetty/jetty-start-7.0.0.jar:/Users/sychan/src/jars/lib/jars/jetty/jetty-all-7.0.0.jar:/Users/sychan/src/jars/lib/jars/servlet/servlet-api-2.5.jar 	-DKB_DEPLOYMENT_CONFIG=$script_dir/../deploy.cfg -Djetty.port=5000 org.eclipse.jetty.start.Main jetty.xml
