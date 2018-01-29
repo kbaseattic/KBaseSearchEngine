ae20f4bfdcf5325f41526518fa427021e0773ba0
Arfath Pasha <arfathpasha@gmail.com>
2018-01-29 10:00:40 -0500

    add updateEvent method

--- lib/src/kbasesearchengine/events/handler/EventHandler.java
+++ lib/src/kbasesearchengine/events/handler/EventHandler.java
@@ -1,5 +1,6 @@
 package kbasesearchengine.events.handler;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Iterator;
 import java.util.List;
@@ -8,11 +9,14 @@ import java.util.Set;
 
 import kbasesearchengine.common.GUID;
 import kbasesearchengine.events.ChildStatusEvent;
+import kbasesearchengine.events.StatusEvent;
 import kbasesearchengine.events.StoredStatusEvent;
 import kbasesearchengine.events.exceptions.IndexingException;
 import kbasesearchengine.events.exceptions.IndexingExceptionUncheckedWrapper;
 import kbasesearchengine.events.exceptions.RetriableIndexingException;
 import kbasesearchengine.events.exceptions.RetriableIndexingExceptionUncheckedWrapper;
+import kbasesearchengine.events.exceptions.UnprocessableEventIndexingException;
+import us.kbase.common.service.JsonClientException;
 
 /** An interface for handling search events. The interface abstracts away event source specific
  * operations.
@@ -21,7 +25,7 @@ import kbasesearchengine.events.exceptions.RetriableIndexingExceptionUncheckedWr
  *
  */
 public interface EventHandler {
-    
+
     /** Get the storage code for the storage implementation with which this event handler is
      * associated.
      * @return the storage code.
@@ -41,7 +45,7 @@ public interface EventHandler {
      */
     Iterable<ChildStatusEvent> expand(StoredStatusEvent event)
             throws IndexingException, RetriableIndexingException;
-    
+
     /** The equivalent of {@link #load(List, Path) load(Arrays.asList(guid), tempfile)}
      * @param guid the globally unique ID of the source object to load.
      * @param file a file in which to store the object's data, which is expected to exist.
@@ -50,7 +54,7 @@ public interface EventHandler {
      * @throws RetriableIndexingException if a retriable error occurred loading the data.
      */
     SourceData load(GUID guid, Path file) throws IndexingException, RetriableIndexingException;
-    
+
     /** Load an object's data from a remote source. The target object may need to be specified
      * as a path from an accessible object. If the target object is accessible only one entry is
      * expected in the guids field.
@@ -87,4 +91,15 @@ public interface EventHandler {
      * @return true if the event is expandable, false otherwise.
      */
     boolean isExpandable(StoredStatusEvent parentEvent);
+
+    /** Takes the specified event and returns a new event object that is a clone
+     * of the specified event and that reflects the latest state of the object
+     * that it is an event for.
+     *
+     * @param ev event to update
+     * @return a new updated event
+     * @throws UnprocessableEventIndexingException
+     */
+    StatusEvent updateEvent(StatusEvent ev)
+            throws IndexingException, RetriableIndexingException;
 }
