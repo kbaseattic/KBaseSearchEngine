cdd7eb1c4d9108b7a67faaeb689f39713cce4690
JamesJeffryes <jamesgjeffryes@gmail.com>
2018-02-02 12:03:09 -0600

    tests for new types

--- test/src/kbasesearchengine/test/parse/ObjectParserTest.java
+++ test/src/kbasesearchengine/test/parse/ObjectParserTest.java
@@ -253,17 +253,234 @@ public class ObjectParserTest {
         }
     }
 
+    @Test
+    /**
+     * Testing parsing MediaCompound object
+     *
+     * @throws Exception
+     */
+    public void parseSubObjectsMediaCompoundTest() throws Exception {
+
+        final String jsonResource = "media01";
+        final String type = "MediaCompound";
+        final String guid = "WS:1/1/1";
+
+        final Map<GUID, String> guidToJson = parseSubObjects(type, jsonResource, guid);
+
+        assertThat(guidToJson.size(), is(3));
+
+        final List<String> expectedIds = Arrays.asList("cpd00027", "cpd00013",
+                "cpd00009");
+        int key_index = 0;
+        for (GUID guidKey : guidToJson.keySet()) {
+            assertThat(guidKey.getSubObjectId(), is(expectedIds.get(key_index)));
+            assertThat(guidKey.getSubObjectType(), is("MediaCompound"));
+            assertThat(guidKey.getStorageCode(), is("WS"));
+            assertThat(guidKey.getVersion(), is(1));
+            assertThat(guidKey.getAccessGroupId(), is(1));
+            assertThat(guidKey.getAccessGroupObjectId(), is("1"));
+            key_index++;
+        }
+
+        final List<String> expectedKeys = Arrays.asList("inchikey", "concentration",
+                "minFlux", "maxFlux", "id", "name");
+        final List<Double> expectedFlux = Arrays.asList( 100.0, 10.0, 50.0);
+        final Double expectedConcentration = 0.001;
+        int value_index = 0;
+        for (String jsonString : guidToJson.values()) {
+            @SuppressWarnings("unchecked")
+            HashMap<String, Object> result = new ObjectMapper().readValue(jsonString,
+                    HashMap.class);
+            assertThat(expectedKeys.containsAll(result.keySet()), is(true));
+            assertThat(result.get("id"), is(expectedIds.get(value_index)));
+            assertThat(result.get("maxFlux"), is(expectedFlux.get(value_index)));
+            assertThat(result.get("minFlux"), is(-expectedFlux.get(value_index)));
+            assertThat(result.get("concentration"), is(expectedConcentration));
+            value_index++;
+        }
+    }
+
+    @Test
+    /**
+     * Testing parsing FBAModel object
+     *
+     * @throws Exception
+     */
+    public void parseObjectsModelTest() throws Exception {
+
+        final String jsonResource = "fbamodel01";
+        final String type = "FBAModel";
+        final String guid = "WS:1/1/1";
+
+        final Map<GUID, String> guidToJson = parseSubObjects(type, jsonResource, guid);
+
+        assertThat(guidToJson.size(), is(1));
+        final String jsonString = guidToJson.values().iterator().next();
+        @SuppressWarnings("unchecked")
+        HashMap<String, Object> result = new ObjectMapper().readValue(jsonString, HashMap.class);
+
+        HashMap<String, Object> expectedResult = new HashMap<String, Object>();
+        expectedResult.put("id", "rast_test");
+        expectedResult.put("source", "KBase");
+        expectedResult.put("modelcompartments", 2);
+        expectedResult.put("modelcompounds", 3);
+        expectedResult.put("modelreactions", 3);
+        expectedResult.put("name", "E. coli");
+        expectedResult.put("type", "GenomeScale");
+        expectedResult.put("genome_ref", "1/2/1");
+
+        assertThat(expectedResult, is(result));
+    }
+
+    @Test
+    /**
+     * Testing parsing ModelCompound object
+     *
+     * @throws Exception
+     */
+    public void parseSubObjectsModelCompoundTest() throws Exception {
+
+        final String jsonResource = "fbamodel01";
+        final String type = "ModelCompound";
+        final String guid = "WS:1/1/1";
+
+        final Map<GUID, String> guidToJson = parseSubObjects(type, jsonResource, guid);
+
+        assertThat(guidToJson.size(), is(3));
+
+        final List<String> expectedIds = Arrays.asList("cpd00001_c0", "cpd02154_c0",
+                "cpd00013_c0");
+        int key_index = 0;
+        for (GUID guidKey : guidToJson.keySet()) {
+            assertThat(guidKey.getSubObjectId(), is(expectedIds.get(key_index)));
+            assertThat(guidKey.getSubObjectType(), is("ModelCompound"));
+            assertThat(guidKey.getStorageCode(), is("WS"));
+            assertThat(guidKey.getVersion(), is(1));
+            assertThat(guidKey.getAccessGroupId(), is(1));
+            assertThat(guidKey.getAccessGroupObjectId(), is("1"));
+            key_index++;
+        }
+
+        final List<String> expectedKeys = Arrays.asList("inchikey", "formula",
+                "aliases", "id", "name");
+        final List<String> expectedFormula = Arrays.asList("H2O", "C5H7N2O4", "H4N");
+        int value_index = 0;
+        for (String jsonString : guidToJson.values()) {
+            @SuppressWarnings("unchecked")
+            HashMap<String, Object> result = new ObjectMapper().readValue(jsonString,
+                    HashMap.class);
+            assertThat(expectedKeys.containsAll(result.keySet()), is(true));
+            assertThat(result.get("id"), is(expectedIds.get(value_index)));
+            assertThat(result.get("formula"), is(expectedFormula.get(value_index)));
+            value_index++;
+        }
+    }
+
+    @Test
+    /**
+     * Testing parsing ModelReaction object
+     *
+     * @throws Exception
+     */
+    public void parseSubObjectsModelReactionTest() throws Exception {
+
+        final String jsonResource = "fbamodel01";
+        final String type = "ModelReaction";
+        final String guid = "WS:1/1/1";
+
+        final Map<GUID, String> guidToJson = parseSubObjects(type, jsonResource, guid);
+
+        assertThat(guidToJson.size(), is(3));
+
+        final List<String> expectedIds = Arrays.asList("rxn02282_c0", "rxn02201_c0",
+                "rxn00351_c0");
+        int key_index = 0;
+        for (GUID guidKey : guidToJson.keySet()) {
+            assertThat(guidKey.getSubObjectId(), is(expectedIds.get(key_index)));
+            assertThat(guidKey.getSubObjectType(), is("ModelReaction"));
+            assertThat(guidKey.getStorageCode(), is("WS"));
+            assertThat(guidKey.getVersion(), is(1));
+            assertThat(guidKey.getAccessGroupId(), is(1));
+            assertThat(guidKey.getAccessGroupObjectId(), is("1"));
+            key_index++;
+        }
+
+        final List<String> expectedKeys = Arrays.asList("pathway", "aliases", "id", 
+                "name");
+        final List<String> expectedName = Arrays.asList("N-Formimino-L-aspartate iminohydrolase_c0", 
+                "2-amino-4-hydroxy-6-hydroxymethyl-7,8-dihydropteridine-diphosphate:4-aminobenzoate 2-amino-4-hydroxydihydropteridine-6-methenyltransferase_c0", 
+                "gamma-L-glutamyl-L-cysteine:glycine ligase (ADP-forming)_c0");
+        int value_index = 0;
+        for (String jsonString : guidToJson.values()) {
+            @SuppressWarnings("unchecked")
+            HashMap<String, Object> result = new ObjectMapper().readValue(jsonString,
+                    HashMap.class);
+            assertThat(expectedKeys.containsAll(result.keySet()), is(true));
+            assertThat(result.get("id"), is(expectedIds.get(value_index)));
+            assertThat(result.get("name"), is(expectedName.get(value_index)));
+            value_index++;
+        }
+    }
+
+    @Test
+    /**
+     * Testing parsing ModelReactionProteinSubunit object
+     *
+     * @throws Exception
+     */
+    public void parseSubObjectsModelReactionProteinSubunitTest() throws Exception {
+
+        final String jsonResource = "fbamodel01";
+        final String type = "ModelReactionProteinSubunit";
+        final String guid = "WS:1/1/1";
+
+        final Map<GUID, String> guidToJson = parseSubObjects(type, jsonResource, guid);
+
+        assertThat(guidToJson.size(), is(3));
+
+        final List<String> expectedIds = Arrays.asList("allantoate deiminase (EC 3.5.3.9)", 
+                "Dihydropteroate synthase (EC 2.5.1.15)",
+                "Glutathione synthetase (EC 6.3.2.3)");
+        int key_index = 0;
+        for (GUID guidKey : guidToJson.keySet()) {
+            assertThat(guidKey.getSubObjectId(), is(expectedIds.get(key_index)));
+            assertThat(guidKey.getSubObjectType(), is("ModelReactionProteinSubunit"));
+            assertThat(guidKey.getStorageCode(), is("WS"));
+            assertThat(guidKey.getVersion(), is(1));
+            assertThat(guidKey.getAccessGroupId(), is(1));
+            assertThat(guidKey.getAccessGroupObjectId(), is("1"));
+            key_index++;
+        }
+
+        final List<String> expectedKeys = Arrays.asList("role", "feature_refs");
+        final List<List<String>> expectedName = Arrays.asList(
+                Arrays.asList("~/genome/features/id/eco_from_assembly.CDS.522"), 
+                Arrays.asList("~/genome/features/id/eco_from_assembly.CDS.3263"), 
+                Arrays.asList("~/genome/features/id/eco_from_assembly.CDS.3031"));
+        int value_index = 0;
+        for (String jsonString : guidToJson.values()) {
+            @SuppressWarnings("unchecked")
+            HashMap<String, Object> result = new ObjectMapper().readValue(jsonString,
+                    HashMap.class);
+            assertThat(expectedKeys.containsAll(result.keySet()), is(true));
+            assertThat(result.get("role"), is(expectedIds.get(value_index)));
+            assertThat(result.get("feature_refs"), is(expectedName.get(value_index)));
+            value_index++;
+        }
+    }
+
     /**
      * Helper method for ObjectParser.parseSubObjects tests
      * 
      * @param type
      * @param jsonResource
      * @param guidString
+     * @param rule_version
      * @return
      * @throws Exception
      */
     public static Map<GUID, String> parseSubObjects(String type, String jsonResource,
-            String guidString) throws Exception {
+            String guidString, int rule_version) throws Exception {
 
         final GUID guid = new GUID(guidString);
 
@@ -278,10 +495,24 @@ public class ObjectParserTest {
         final SourceData obj = SourceData.getBuilder(data, name, creator).build();
         final File rulesFile = new File("resources/types/" + type + ".yaml");
         final ObjectTypeParsingRules parsingRules = ObjectTypeParsingRulesFileParser
-                .fromFile(rulesFile).get(0);
+                .fromFile(rulesFile).get(rule_version);
 
         final Map<GUID, String> guidToJson = ObjectParser.parseSubObjects(obj, guid, parsingRules);
 
         return guidToJson;
+    }
+
+    /**
+     * Helper method for ObjectParser.parseSubObjects tests
+     * 
+     * @param type
+     * @param jsonResource
+     * @param guidString
+     * @return
+     * @throws Exception
+     */
+    public static Map<GUID, String> parseSubObjects(String type, String jsonResource,
+            String guidString) throws Exception {
+        return parseSubObjects(type, jsonResource, guidString, 0);
     }
 }
