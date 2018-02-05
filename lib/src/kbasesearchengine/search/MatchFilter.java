19790a9794d67c9a10c415d82a80f6563dc8099b
Gavin Price <gaprice@lbl.gov>
2018-02-04 21:26:05 -0800

    Add MatchFilter javadoc

--- lib/src/kbasesearchengine/search/MatchFilter.java
+++ lib/src/kbasesearchengine/search/MatchFilter.java
@@ -10,9 +10,14 @@ import com.google.common.base.Optional;
 
 import kbasesearchengine.tools.Utils;
 
+/**
+ * Describes what objects in {@link IndexingStorage} a search should return.
+ * 
+ * @author gaprice@lbl.gov
+ * @author rsutormin@lbl.gov
+ *
+ */
 public class MatchFilter {
-    
-    //TODO NNOW JAVADOC
     
     private final boolean excludeSubObjects;
     private final Optional<String> fullTextInAll;
@@ -39,30 +44,52 @@ public class MatchFilter {
         this.isSourceTagsBlacklist = isSourceTagsBlacklist;
     }
 
+    /** True if sub objects should be excluded from the search, default false.
+     * @return true if sub objects should be excluded.
+     */
     public boolean isExcludeSubObjects() {
         return excludeSubObjects;
     }
 
+    /** A text string to match across all fields of an object, if present.
+     * @return the text string.
+     */
     public Optional<String> getFullTextInAll() {
         return fullTextInAll;
     }
 
+    /** An object name to match across objects, if present.
+     * @return the object name.
+     */
     public Optional<String> getObjectName() {
         return objectName;
     }
 
+    /** A time range to match across objects, if present.
+     * @return the time range.
+     */
     public Optional<MatchValue> getTimestamp() {
         return timestamp;
     }
 
+    /** A set of matches on object keys to search on.
+     * @return the matches.
+     */
     public Map<String, MatchValue> getLookupInKeys() {
         return lookupInKeys;
     }
 
+    /** Tags applied to objects at the data source to apply to the search.
+     * @return data source tags.
+     */
     public Set<String> getSourceTags() {
         return sourceTags;
     }
 
+    /** Returns true if the data source tags is a blacklist, or false if a whitelist
+     * (the default).
+     * @return whether the data source tags is a whitelist or blacklist.
+     */
     public boolean isSourceTagsBlacklist() {
         return isSourceTagsBlacklist;
     }
@@ -142,10 +169,17 @@ public class MatchFilter {
         return true;
     }
 
+    /** Get a builder for a {@link MatchFilter}.
+     * @return
+     */
     public static Builder getBuilder() {
         return new Builder();
     }
     
+    /** A {@link MatchFilter} builder.
+     * @author gaprice@lbl.gov
+     *
+     */
     public static class Builder {
         
         private boolean excludeSubObjects = false;
@@ -158,31 +192,57 @@ public class MatchFilter {
         
         private Builder() {}
 
+        /** Set whether subobjects should be returned in the search results, default false.
+         * @param excludeSubObjects whether subobjects should be returned.
+         * @return this builder.
+         */
         public Builder withExcludeSubObjects(final boolean excludeSubObjects) {
             this.excludeSubObjects = excludeSubObjects;
             return this;
         }
         
+        /** Add a text string that is compared across all object fields for matches when searching.
+         * @param text the text string.
+         * @return this builder.
+         */
         public Builder withNullableFullTextInAll(final String text) {
             this.fullTextInAll = text;
             return this;
         }
         
+        /** Add an object name to search against.
+         * @param objectName the object name.
+         * @return this builder.
+         */
         public Builder withNullableObjectName(final String objectName) {
             this.objectName = objectName;
             return this;
         }
         
+        /** Add a range that constrains the creation date of the returned objects.
+         * @param value the timestamp range.
+         * @return this builder.
+         */
         public Builder withNullableTimestamp(final MatchValue value) {
             this.timestamp = value;
             return this;
         }
         
+        /** Add a key / value pair to search on.
+         * @param keyName the object key to match.
+         * @param value the value of the key to match.
+         * @return this builder.
+         */
         public Builder withLookupInKey(final String keyName, final String value) {
             Utils.notNullOrEmpty(value, "value cannot be null or whitespace only");
             return withLookupInKey(keyName, new MatchValue(value));
         }
         
+        /** Add a key / value pair to search on.
+         * @param keyName the object key to match.
+         * @param value the value of the key to match.
+         * @return this builder.
+         */
         public Builder withLookupInKey(final String keyName, final MatchValue value) {
             Utils.notNullOrEmpty(keyName, "key cannot be null or whitespace only");
             Utils.nonNull(value, "value");
@@ -190,17 +250,29 @@ public class MatchFilter {
             return this;
         }
         
+        /** Add a tag applied at the data source to match against in the search.
+         * @param sourceTag the data source tag.
+         * @return this builder.
+         */
         public Builder withSourceTag(final String sourceTag) {
             Utils.notNullOrEmpty(sourceTag, "sourceTag cannot be null or whitespace only");
             sourceTags.add(sourceTag);
             return this;
         }
         
+        /** Specify whether the data source tags should be applied as a blacklist or a whitelist,
+         * the default.
+         * @param isBlacklist true if the data source tags should be treated as a blacklist.
+         * @return this builder.
+         */
         public Builder withIsSourceTagsBlackList(final boolean isBlacklist) {
             isSourceTagsBlacklist = isBlacklist;
             return this;
         }
         
+        /** Build the {@link MatchFilter}.
+         * @return this builder. the new {@link MatchFilter}.
+         */
         public MatchFilter build() {
             return new MatchFilter(excludeSubObjects, fullTextInAll, objectName, timestamp,
                     lookupInKeys, sourceTags, isSourceTagsBlacklist);
