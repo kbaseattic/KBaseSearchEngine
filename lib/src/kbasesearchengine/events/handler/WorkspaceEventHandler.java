8c9118cdc10d6669dc67e1883549eef4c6ab9562
U Ganapathy <umaganapathyswork@gmail.com>
2018-02-08 13:27:42 -0500

    Removed an unused import

--- lib/src/kbasesearchengine/events/handler/WorkspaceEventHandler.java
+++ lib/src/kbasesearchengine/events/handler/WorkspaceEventHandler.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 import org.joda.time.format.DateTimeFormat;
