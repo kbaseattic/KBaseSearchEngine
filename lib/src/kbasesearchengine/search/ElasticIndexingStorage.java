7d03e2da133b0999320c0e300f570101f0844f5d
Gavin Price <gaprice@lbl.gov>
2018-02-19 20:43:45 -0800

    Refactor Elastic sorting code
    Can now specify any object property to sort on
    
    Note that object names will need to be changed to the keyword type and
    reindexed for sorting on object names to work.

--- lib/src/kbasesearchengine/search/ElasticIndexingStorage.java
+++ lib/src/kbasesearchengine/search/ElasticIndexingStorage.java
@@ -42,7 +42,7 @@ import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 
 import com.google.common.base.Optional;
-
+import com.google.common.collect.ImmutableBiMap;
 import com.google.common.collect.ImmutableMap;
 
 import kbasesearchengine.common.GUID;
@@ -84,7 +84,6 @@ public class ElasticIndexingStorage implements IndexingStorage {
     private static final String SEARCH_OBJ_TYPE = "otype";
     private static final String SEARCH_OBJ_TYPE_VER = "otypever";
 
-
     //readable names
     private static final String R_OBJ_GUID = "guid";
     private static final String R_OBJ_TIMESTAMP = "timestamp";
@@ -110,7 +109,8 @@ public class ElasticIndexingStorage implements IndexingStorage {
     private static final String R_SEARCH_OBJ_TYPE = "type";
     private static final String R_SEARCH_OBJ_TYPE_VER = "type_ver";
 
-    private static final ImmutableMap<String, String> readableNames = ImmutableMap.<String,String>builder()
+    private static final ImmutableBiMap<String, String> READABLE_NAMES = ImmutableBiMap.
+            <String,String>builder()
             .put(OBJ_GUID, R_OBJ_GUID)
             .put(OBJ_TIMESTAMP, R_OBJ_TIMESTAMP)
             .put(OBJ_PROV_COMMIT_HASH, R_OBJ_PROV_COMMIT_HASH)
@@ -1231,11 +1231,12 @@ public class ElasticIndexingStorage implements IndexingStorage {
         return b.build();
     }
 
-    private String getReadableKeyNames(final String key, final GUID guid) throws IllegalStateException{
+    private String getReadableKeyNames(final String key, final GUID guid)
+            throws IllegalStateException{
         if (key.startsWith("key.")) {
             return stripKeyPrefix(key);
-        } else if(readableNames.containsKey(key)) {
-            return readableNames.get(key);
+        } else if(READABLE_NAMES.containsKey(key)) {
+            return READABLE_NAMES.get(key);
         } else {
             //this should not happen. Untested
             String message = "Object with guid " + guid.toString() + " has unexpected key: " + key;
@@ -1524,7 +1525,7 @@ public class ElasticIndexingStorage implements IndexingStorage {
         int pgCount = pg == null || pg.count == null ? 50 : pg.count;
         Pagination pagination = new Pagination(pgStart, pgCount);
         if (sorting == null || sorting.isEmpty()) {
-            final SortingRule sr = SortingRule.getStandardPropertyBuilder(OBJ_TIMESTAMP).build();
+            final SortingRule sr = SortingRule.getStandardPropertyBuilder(R_OBJ_TIMESTAMP).build();
             sorting = Arrays.asList(sr);
         }
         FoundHits ret = new FoundHits();
@@ -1546,20 +1547,12 @@ public class ElasticIndexingStorage implements IndexingStorage {
         doc.put("from", pagination.start);
         doc.put("size", pagination.count);
 
-        boolean loadObjects = pp != null && (pp.objectInfo || pp.objectData || pp.objectKeys || pp.objectHighlight);
+        boolean loadObjects = pp != null &&
+                (pp.objectInfo || pp.objectData || pp.objectKeys || pp.objectHighlight);
         if (!loadObjects) {
             doc.put("_source", Arrays.asList("guid"));
         }
-        List<Object> sort = new ArrayList<>();
-        doc.put("sort", sort);
-        for (final SortingRule sr : sorting) {
-            final String keyProp = sr.isKeyProperty() ? getKeyProperty(sr.getKeyProperty().get()) :
-                    sr.getStandardProperty().get();
-
-            final Map<String, Object> sortOrderWrapper = ImmutableMap.of(
-                    keyProp, ImmutableMap.of("order", sr.isAscending() ? "asc" : "desc"));
-            sort.add(sortOrderWrapper);
-        }
+        doc.put("sort", createSortQuery(sorting));
 
         validateObjectTypes(objectTypes);
 
@@ -1610,6 +1603,27 @@ public class ElasticIndexingStorage implements IndexingStorage {
             }
         }
         return ret;
+    }
+
+    private List<Object> createSortQuery(final List<SortingRule> sorting) {
+        final List<Object> sort = new ArrayList<>();
+        for (final SortingRule sr : sorting) {
+            final Map<String, String> order = ImmutableMap.of
+                    ("order", sr.isAscending() ? "asc" : "desc");
+            final Map<String, Object> sortWrapper;
+            if (sr.isKeyProperty()) {
+                sortWrapper = ImmutableMap.of(getKeyProperty(sr.getKeyProperty().get()), order);
+            } else {
+                if (!READABLE_NAMES.inverse().containsKey(sr.getStandardProperty().get())) {
+                    throw new IllegalArgumentException("Unknown object property " +
+                            sr.getStandardProperty().get());
+                }
+                sortWrapper = ImmutableMap.of(
+                        READABLE_NAMES.inverse().get(sr.getStandardProperty().get()), order);
+            }
+            sort.add(sortWrapper);
+        }
+        return sort;
     }
 
     private String getKeyProperty(final String keyName) {
