0e0dc90a58c31d95d0c34d06b15bf2ed3805376d
Gavin Price <gaprice@lbl.gov>
2018-02-27 18:40:27 -0800

    Missing jdoc

--- lib/src/kbasesearchengine/parse/ContigLocationException.java
+++ lib/src/kbasesearchengine/parse/ContigLocationException.java
@@ -1,5 +1,9 @@
 package kbasesearchengine.parse;
 
+/** An error thrown when data regarding locations on a contig are missing or erroneous.
+ * @author gaprice@lbl.gov
+ *
+ */
 public class ContigLocationException extends ObjectParseException {
 
     private static final long serialVersionUID = 1L;
