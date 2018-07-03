e75dda39937209b2d48fe82f27503c96a04e3198
dianezheng <dianezheng@dianezhengs-MacBook-Pro.local>
2018-07-03 15:27:18 -0700

    intellij

--- test/src/kbasesearchengine/test/integration/SearchAPIIntegrationTest.java
+++ test/src/kbasesearchengine/test/integration/SearchAPIIntegrationTest.java
@@ -26,6 +26,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.HashSet;
+
 import java.util.stream.Collectors;
 
 import kbasesearchengine.events.handler.CloneableWorkspaceClientImpl;
