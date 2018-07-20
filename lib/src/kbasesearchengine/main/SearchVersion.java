a3eeaa9f6675812fdcd2ff827099cda9963d872b
dianezheng <dianezheng@dianezhengs-MBP.dhcp.lbl.gov>
2018-07-20 10:08:55 -0700

    update version

--- lib/src/kbasesearchengine/main/SearchVersion.java
+++ lib/src/kbasesearchengine/main/SearchVersion.java
@@ -13,6 +13,6 @@ package kbasesearchengine.main;
 public class SearchVersion {
     
     /** The search code version. */
-    public static final String VERSION = "0.1.1";
+    public static final String VERSION = "0.2.2";
 
 }
