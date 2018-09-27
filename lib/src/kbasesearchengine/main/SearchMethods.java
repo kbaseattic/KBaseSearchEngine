aa0b45181e94f439a19d8b4a987fed9f34b1a96d
dianezheng <dianezheng@dianezhengs-MBP.dhcp.lbl.gov>
2018-09-27 15:15:06 -0700

    log query

--- lib/src/kbasesearchengine/main/SearchMethods.java
+++ lib/src/kbasesearchengine/main/SearchMethods.java
@@ -35,6 +35,8 @@ import kbasesearchengine.search.MatchFilter.Builder;
 import kbasesearchengine.system.IndexingRules;
 import kbasesearchengine.system.ObjectTypeParsingRules;
 import kbasesearchengine.system.TypeStorage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import us.kbase.common.service.UObject;
 
 public class SearchMethods implements SearchInterface {
@@ -289,6 +291,12 @@ public class SearchMethods implements SearchInterface {
         }
         ret.withTotal((long)hits.total);
         ret.withSearchTime(System.currentTimeMillis() - t1);
+
+        getLogger().info("user: {}", user );
+        getLogger().info("query: {}, with postProcessing: {}", params.getMatchFilter().toString(), params.getPostProcessing().toString() );
+        getLogger().info("Number of hits returned: {}", ret.getTotal());
+
+
         return ret;
     }
 
@@ -355,6 +363,10 @@ public class SearchMethods implements SearchInterface {
             ret.put(typeName, td);
         }
         return ret;
+    }
+
+    private Logger getLogger() {
+        return LoggerFactory.getLogger(NarrativeInfoDecorator.class);
     }
 
     private static String guessUIName(String id) {
